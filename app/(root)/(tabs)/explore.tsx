82096a565943215f014041499d9b644c3bd31cdc
Kasun Gunawardana <kasun4it@gmail.com>
2025-02-22 11:22:15 +0530

    initial commit and nativewind added

---
+++ app/(root)/(tabs)/explore.tsx
@@ -0,0 +1,17 @@
+import {View, Text} from 'react-native'
+import React from 'react'
+
+const Explore = () => {
+    return (
+        <View
+            style={{
+                flex: 1,
+                justifyContent: "center",
+                alignItems: "center",
+            }}
+        >
+            <Text>Explore</Text>
+        </View>
+    )
+}
+export default Explore
---
+++ app/(root)/properties/[id].tsx
@@ -0,0 +1,19 @@
+import {View, Text} from 'react-native'
+import React from 'react'
+import {useLocalSearchParams} from "expo-router";
+
+const Property = () => {
+    const { id } = useLocalSearchParams()
+    return (
+        <View
+            style={{
+                flex: 1,
+                justifyContent: "center",
+                alignItems: "center",
+            }}
+        >
+            <Text>Property {id}</Text>
+        </View>
+    )
+}
+    export default Property
